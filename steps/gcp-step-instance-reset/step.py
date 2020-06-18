525d52230330e02fee12af2b5f17027d5e1481fe
Kenaz <kenaz@puppetlabs.com>
2020-06-18 11:22:16 -0700

    Adding a couple more instance steps

---
+++ steps/gcp-step-instance-reset/step.py
@@ -0,0 +1,52 @@
+import googleapiclient.discovery
+
+from google.oauth2 import service_account
+from nebula_sdk import Interface, Dynamic as D
+import json
+
+def slice(orig, keys):
+    return {key: orig[key] for key in keys if key in orig}
+
+def do_reset_instance(compute, project_id, zone, name):
+    print('Resetting instance {0}'.format(name))
+    result = compute.instances().reset(project=project_id, zone=zone, instance=name).execute()
+    return result['items'] if 'items' in result else []
+
+def reset_instances(instances):
+    # For security purposes we whitelist the keys that can be fed in to the
+    # google oauth library. This prevents workflow users from feeding arbitrary
+    # data in to that library.
+    service_account_info_keys = [
+        "type",
+        "project_id",
+        "private_key_id",
+        "private_key",
+        "client_email",
+        "client_id",
+        "auth_uri",
+        "token_uri",
+        "auth_provider_x509_cert_url",
+        "client_x509_cert_url",
+    ]
+
+    zone = relay.get(D.google.zone)
+
+    if not zone:
+        print("Missing `google.zone` parameter on step configuration.")
+        exit(1)
+
+    # TODO: How to validate all the required data is present here?
+    service_account_info=slice(json.loads(relay.get(D.google.service_account_info)['serviceAccountKey']), service_account_info_keys)
+
+    credentials = service_account.Credentials.from_service_account_info(service_account_info)
+    compute = googleapiclient.discovery.build("compute", "v1", credentials=credentials)
+
+    for instance in instances:
+        if isinstance(instance, dict):
+            do_reset_instance(compute, project_id=credentials.project_id, zone=zone, name=instance["name"])
+        else: 
+            do_reset_instance(compute, project_id=credentials.project_id, zone=zone, name=instance)
+
+if __name__ == "__main__":
+    relay = Interface()
+    reset_instances(relay.get(D.instances))
